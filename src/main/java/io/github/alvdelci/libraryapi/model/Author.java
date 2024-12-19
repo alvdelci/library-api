e90c09fac543d47add719ed7886b8a51bbdeb0b8
alvdelci <delcimarioalvesdev@gmail.com>
2024-12-19 18:55:01 -0300

    feat: Mapeando entidade Author

---
+++ src/main/java/io/github/alvdelci/libraryapi/model/Author.java
@@ -0,0 +1,33 @@
+package io.github.alvdelci.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author", schema = "public")
+@Getter
+@Setter //Gera os getters e setters em tempo de compilacao
+public class Author {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "birthday", nullable = false)
+    private LocalDate birthday;
+
+    @Column(name = "nationality", length = 50, nullable = false)
+    private String nationality;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+}
