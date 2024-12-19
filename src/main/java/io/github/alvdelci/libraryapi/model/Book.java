9216be8b7f5106ee44143c808919da1bf2681640
alvdelci <delcimarioalvesdev@gmail.com>
2024-12-19 18:54:52 -0300

    feat: Mapeando entidade Book

---
+++ src/main/java/io/github/alvdelci/libraryapi/model/Book.java
@@ -0,0 +1,38 @@
+package io.github.alvdelci.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+@Data //Gera os getters e setters, toString, noArgsConstructor e outros. Isso substitui as anotacao especificas
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "title", length = 150, nullable = false)
+    private String title;
+
+    @Column(name = "publication_date")
+    private LocalDate publicationDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", length = 30, nullable = false)
+    private BookGender gender;
+
+    @Column(name = "price", precision = 18, scale = 2)
+    private Double price;
+    //private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
