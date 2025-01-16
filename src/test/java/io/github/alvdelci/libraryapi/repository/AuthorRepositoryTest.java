05ac24cf4e7c8507bf3c750562854629bd092880
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-16 18:26:03 -0300

    test: Adicionando tests das demais operacoes no author

--- src/test/java/io/github/alvdelci/libraryapi/repository/AuthorRepositoryTest.java
+++ src/test/java/io/github/alvdelci/libraryapi/repository/AuthorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -42,5 +43,29 @@ public class AuthorRepositoryTest {
 
             repository.save(foundAuthor);
         }
+    }
+
+    @Test
+    public void listTest() {
+        List<Author> list = repository.findAll();
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest() {
+        System.out.println("Contagem de cadastros: " + repository.count());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        var id = UUID.fromString("29ea52ac-4876-4fad-bd03-d944a436bc08");
+        repository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest() { //delete by object
+        var id = UUID.fromString("bbcb6d14-e558-4b2e-a7be-77f867279118");
+        var jose = repository.findById(id).get();
+        repository.delete(jose);
     }
 }
